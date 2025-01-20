571856474cd1e16e6d0204deb4052e8698327971
MatheusBrunoAlmeida <manancialsup@hotmail.com>
2025-01-20 17:46:36 -0300

    add hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,19 @@ export default function Home() {
       </div>
 
       <div className="w-full p-16 gap-4 flex lg:items-center flex-col">
-        <Card className="flex justify-start shadow-md gap-4 w-full lg:w-1/4 p-4 items-center">
+        <Card className="flex justify-start hover:bg-gray-50 cursor-pointer shadow-md gap-4 w-full lg:w-1/4 p-4 items-center">
           <div className="w-2 h-2 bg-blue-400 rounded-full"></div>
 
           <h1>40 dias com Jesus</h1>
         </Card>
 
-        <Card className="flex justify-start shadow-md gap-4 w-full lg:w-1/4 p-4 items-center">
+        <Card className="flex justify-start hover:bg-gray-50 cursor-pointer shadow-md gap-4 w-full lg:w-1/4 p-4 items-center">
           <div className="w-2 h-2 bg-red-600 rounded-full"></div>
 
           <h1>Plano 2</h1>
         </Card>
 
-        <Card className="flex justify-start shadow-md gap-4 w-full lg:w-1/4 p-4 items-center">
+        <Card className="flex justify-start hover:bg-gray-50 cursor-pointer shadow-md gap-4 w-full lg:w-1/4 p-4 items-center">
           <div className="w-2 h-2 bg-green-400 rounded-full"></div>
 
           <h1>Plano 3</h1>
